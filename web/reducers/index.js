f585a9912b8865d28f51e4a181d642bf7d1a5c52
malnaidu <malnaidu@uw.edu>
2016-07-26 12:28:16 -0700

    clean up

--- web/reducers/index.js
+++ web/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import sample from './sample';
 
 const rootReducer = combineReducers({
-  sample,
   routing
 });
 
