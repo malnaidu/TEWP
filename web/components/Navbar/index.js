f119ea1eef9ee45fc25e69f206c35fb515d5bb1a
Dave Banan <daveba@zillow.com>
2016-07-26 23:10:50 -0700

    finished up basic styling on the header

--- web/components/Navbar/index.js
+++ web/components/Navbar/index.js
@@ -1,27 +1,34 @@
 import React, {Component} from 'react';
+import FontAwesome from 'react-fontawesome';
 import styles from './Navbar.scss';
 
 export default class Navbar extends Component {
   render() {
     return (
-	<div>
-		<div id="lightBlueheader">
-			<div>
-
+	<div id="container" className={styles.container}>
+		<div id="zillowLogo" className={styles.zillowLogo}>
+			<div id="topText" className={styles.topText}>
+				<span id="zillowBrand"className={styles.zillowBrand}>Zillow</span>
+				<span id="groupText" className={styles.groupText}>GROUP</span>
 			</div>
-
-			<div id="navLinks">
-
-			</div>
-		</div>
-
-		<div id="darkBlueheader" >
-			<div id="techRequest">
+			<div id="bottomText" className={styles.bottomText}>
+				ZEUS
 			</div>
 		</div>
 
-		<div id="zillowLogo">
+		<div id="search" className={styles.search}>
+			<input type="text" placeholder="Search" className={styles.searchInput}></input>
+		</div>
 
+		<div id="navLinks" className={styles.navLinks}>
+			<span id="greeting" className={styles.greeting}>
+				Welcome, Malini Naidu
+			</span>
+			<span id="links" className={styles.links}>
+				<span className={styles.links}>Logout</span>
+				<FontAwesome name='fa-heart' /><span className={styles.links}>Wish List</span>
+				<span className={styles.links}>Cart</span>
+			</span>
 		</div>
     </div>
 
--- web/containers/App/index.js
+++ web/containers/App/index.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
 import styles from './App.scss';
+import Navbar from '../../components/Navbar';
 import Tile from '../../components/Tile';
 
 export default class App extends Component {
   render() {
     return (
       <div className={styles.container}>
-       	 <Tile header="Hardware" 
-  	      		 subHeader= "Featured Laptop"
-  	      		 discription="Macbook Pro"
-  	      		 imageUrl="url"
-  	      		 linkRoute="/computers">
-      	 </Tile>
+        <Navbar />
+        <div className={styles.subHeader}>
+          <span className={styles.subHeaderText}>Tech Request</span>
+        </div>
+        
+         <Tile  header="Hardware" 
+                subHeader= "Featured Laptop"
+                discription="Macbook Pro"
+                imageUrl="url"
+                linkRoute="/computers">
+         </Tile>
       </div>
     );
   }
